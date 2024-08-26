cff74843a6195712bbafa31a6060f487163e314c
rwinklerwilkes <rich.w.sas@gmail.com>
2024-08-25 20:37:15 -0400

    Possibly giving up on Strava - not worth effort?

--- credentials/strava_credential_helper.py
+++ credentials/strava_credential_helper.py
@@ -1,4 +1,6 @@
 import json
+import time
+from stravalib import Client
 
 def get_credentials() -> dict:
     with open('credentials/strava_api_token.json', 'r') as f:
@@ -9,23 +11,17 @@ def set_credentials(credentials:dict) -> None:
     with open('credentials/strava_api_token.json', 'w') as f:
         f.write(json.dumps(credentials))
 
-def get_strava_api_token() -> str:
-    credentials = get_credentials()
-    return credentials['access_token']
-
-def set_strava_api_token(new_token: str) -> None:
-    credentials = get_credentials()
-    credentials['access_token'] = new_token
-    set_credentials(credentials)
-
-def get_strava_refresh_token() -> str:
-    credentials = get_credentials()
-    return credentials['refresh_token']
-
-def strava_refresh_token(new_token: str) -> None:
-    credentials = get_credentials()
-    credentials['refresh_token'] = new_token
-    set_credentials(credentials)
-
 def get_or_refresh_token():
-    pass
+    c = Client()
+    cred = get_credentials()
+
+    if time.time() > cred['expires_at']:
+        refresh_response = c.refresh_access_token(
+            client_id=cred['client_id'], client_secret=cred['client_secret'], refresh_token=cred['refresh_token']
+        )
+
+        cred['access_token'] = refresh_response["access_token"]
+        cred['refresh_token'] = refresh_response["refresh_token"]
+        cred['expires_at'] = refresh_response["expires_at"]
+        set_credentials(cred)
+    return cred
--- utilities/strava.py
+++ utilities/strava.py
@@ -6,5 +6,9 @@
 #   -d refresh_token=ReplaceWithRefreshToken
 
 from credentials import strava_credential_helper as s
+import time
+import requests
 
 credentials = s.get_or_refresh_token()
+access_token = credentials['access_token']
+response = requests.get('https://website.example/id', headers={'Authorization': f'access_token {access_token}'})
